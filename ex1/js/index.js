baa0247206f6549158101a0c6697f082fea71437
Chaz Leong <cleong14@gmail.com>
2015-11-05 19:01:38 -1000

    adding all updated files

--- ex1/js/index.js
+++ ex1/js/index.js
@@ -3,4 +3,16 @@ window.onload = function () {
   // Make it listen for a Click event
     // this event should log to the console the text inside of the <a> tag
     // the event should only log one word per click.
+
+  var navLinks = document.querySelectorAll('a');
+  // var navLinkA = document.querySelectorAll('a');
+
+  for (var i = 0; i < navLinks.length; i++) {
+  	navLinks[i].addEventListener('click', function(event) {
+  		// this.className += ' click';
+  		// console.log(this);
+  		// console.log(event);
+  		console.log(this.innerHTML);
+  	})
+  }
 };
--- ex2/js/index.js
+++ ex2/js/index.js
@@ -3,4 +3,40 @@ window.onload = function () {
   // inspect the .css file
     // when a `child` class is clicked you code will add CSS styles which will show the
     // secret toy element. Click on an element will only show that element's toy, no one elses.
+
+  var childClass = document.querySelectorAll('.child');
+
+  for (var i = 0; i < childClass.length; i++) {
+  	childClass[i].addEventListener('click', function(event) {
+  		var secretToyClass = this.querySelectorAll('.secret-toy')[0];
+  		console.log(this);
+  		secretToyClass.className += ' show';
+  	})
+
+  	childClass[i].addEventListener('mouseout', function(event) {
+  		var secretToyClass = this.querySelectorAll('.secret-toy.show')[0];
+  		console.log(this);
+  		console.log(event);
+  		secretToyClass.className = 'secret-toy'; //????
+  	})
+  }
+
+  // CODE BELOW POTENTIALLY BETTER/PREFERRED METHOD
+
+  // var children = document.getElementsByClassName('child');
+
+  // for (var i = 0; i < children.length; i++) {
+  // 	children[i].addEventListener('click', showSecretToy);
+  // }
+
+  // function showSecretToy (event) {
+  // 	var secretToy = this.querySelector('p');
+
+  // 	if (secretToy.classList.contains('show') === false) {
+  // 		return secretToy.className += ' show';
+  // 	} else {
+  // 		return secretToy.className = 'secret-toy';
+  // 	}
+  // }
+
 };
